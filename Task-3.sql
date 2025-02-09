7a679cdf374f37402408fb3de01bc084cc2f005d
BasutkarSony <basutkarsony14@gmail.com>
2025-02-09 23:46:57 +0530

    Add files via upload

--- Task-3.sql
+++ Task-3.sql
@@ -1,4 +1,5 @@
 use studentmanagement;
+# Finding total score of each student
 select studentid, Name, MathScore, ScienceScore,EnglishScore from students;
 ALTER TABLE students
 add column TotalScore float;
@@ -8,13 +9,16 @@ where studentid>0;
 
 select * from students;
 
+# Identify Top 5 Students by Total Scores
 select name,TotalScore from students
 order by TotalScore desc
 Limit 5;
 
+# Calculate the average score of students who scored above 70 in Math.
 select avg(Mathscore) as Avg_MathScore , avg(TotalScore) as Avg_TotalScore from students
 where Mathscore > 70 and TotalScore >200 and TotalScore<250;
 
+# Find Second-Highest Math Scores
 select name,Max(MathScore) as SecondHighest from students
 where MathScore < (select Max(MathScore) from students)
 group by name, MathScore;
